0634356e47169788c767a80444672cf254fa9e5f
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-17 12:32:03 +0200

    fatto test dell'api per vedere se non ci sono errori

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+// @ts-check
+'use strict';
+
+const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
+
+
+fetch(API_URL)
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        console.log(json);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
