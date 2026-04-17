4c085aa82c11c3cd5b607e4d79a877e6bf115004
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-17 16:31:46 +0200

    creata funzione dove ho inserito la card e stampata in html tramite inner html

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,43 @@
 // @ts-check
 'use strict';
 
+/**
+* @param {{id: number, title: string, date: number, url: string }[]} posts
+*/
+
+function renderCards(posts) {
+    let postsHtml = ''; 
+
+    for (const post of posts) {
+        
+        postsHtml += `
+            <div class="col-12 col-sm-6 col-lg-4">
+                <div class="card rounded-0 h-100 max-dimensione mx-auto custom-card">
+                    <div class="card-body">
+                        <img class="position-absolute top-0 start-50 translate-middle" src="./imgs/pin.svg" alt="puntina che tiene l'immagine">
+                        <div class="img-dimensione"> 
+                            <img src="${post.url}" alt="${post.title}">
+                        </div>
+                        
+                        <p class="card-title pt-1 titolo-immagine">${post.title}</p>
+                        <p class="card-text pb-2 data">${post.date}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    
+    if (cardHtmlElem !== null) {
+        cardHtmlElem.innerHTML = postsHtml;
+    }
+}
+
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
 
 const caricamentoMessaggio = document.querySelector('#caricamento');
 const erroreMessaggio = document.querySelector('#errore');
+const cardHtmlElem = document.querySelector('.card-html');
 
 if (caricamentoMessaggio !== null && erroreMessaggio !== null) {
 
@@ -16,6 +49,7 @@ if (caricamentoMessaggio !== null && erroreMessaggio !== null) {
         .then(json => {
             console.log(json);
             erroreMessaggio.classList.add('d-none'); // aggiunto d-none leggi "README.md 1)" per capire
+            renderCards(json);
         })
         .catch(error => {
             console.error(error);
