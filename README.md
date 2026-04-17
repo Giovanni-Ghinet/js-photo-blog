84a77a0cd5760c06bb8164f733ee909ba8b0e28d
Giovanni Ghinet <ghinetgiovanni88@gmai.com>
2026-04-17 12:52:39 +0200

    implementato caricamento dell'api e messaggio di errore in caso di malfunzionamenti dell'api

---
+++ README.md
@@ -0,0 +1,2 @@
+# spiegazioni
+- 1) ho messo il display none nel then perchè in caso sia vera non mi serve più il p del messaggio errore così lo cancella se lo avessi messo nel finaly come per il messaggio di caricamento lo avrebbe cancellato ma voglio che rimanga in caso di errore 
--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,29 @@
 
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
 
+const caricamentoMessaggio = document.querySelector('#caricamento');
+const erroreMessaggio = document.querySelector('#errore');
 
-fetch(API_URL)
-    .then(response => {
-        return response.json();
-    })
-    .then(json => {
-        console.log(json);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+if (caricamentoMessaggio !== null && erroreMessaggio !== null) {
+
+    caricamentoMessaggio.innerHTML = 'Caricamento';
+    fetch(API_URL)
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            console.log(json);
+            erroreMessaggio.classList.add('d-none'); // aggiunto d-none leggi "README.md 1)" per capire
+        })
+        .catch(error => {
+            console.error(error);
+            erroreMessaggio.innerHTML = 'Si è verificato un errore';
+        })
+        .finally(() => {
+            caricamentoMessaggio.innerHTML = '';
+            caricamentoMessaggio.classList.add('d-none');
+        });
+}
+
+
 
